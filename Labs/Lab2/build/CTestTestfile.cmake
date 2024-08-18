6a1635ed71816b15a966e21fc29e6c27967588f1
JustinPav <145757071+JustinPav@users.noreply.github.com>
2024-08-18 00:43:24 +0000

    Delete Labs/Lab2/build directory

--- Labs/Lab2/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/student/git/41068-JustinPav/Labs/Lab2
-# Build directory: /home/student/git/41068-JustinPav/Labs/Lab2/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
